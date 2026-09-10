5f37ff5792cbf8c7baef9559cabe7ff498361224
mimmofirenze <161960596+mimmofirenze@users.noreply.github.com>
2026-09-10 23:22:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ app.get('/shop/', (req, res) => {
   )
 })
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
